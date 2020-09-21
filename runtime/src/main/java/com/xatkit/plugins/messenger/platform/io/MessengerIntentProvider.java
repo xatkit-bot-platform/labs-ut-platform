78070d8e3ef35c92ec73c7918cdfd301b70c71d9
Taido Purason <taido.purason@ut.ee>
2020-09-21 14:29:10 +0300

    Refactored the Intent Provider. See #2

--- runtime/src/main/java/com/xatkit/plugins/messenger/platform/io/MessengerIntentProvider.java
+++ runtime/src/main/java/com/xatkit/plugins/messenger/platform/io/MessengerIntentProvider.java
@@ -14,45 +14,69 @@ import lombok.val;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.entity.StringEntity;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static fr.inria.atlanmod.commons.Preconditions.checkArgument;
 import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 
+/**
+ * A Messenger-based {@link WebhookEventProvider}.
+ *
+ * @see MessengerPlatform
+ * @see MessengerRestHandler
+ */
 public class MessengerIntentProvider extends WebhookEventProvider<MessengerPlatform, MessengerRestHandler> {
-    MessengerRestHandler restHandler;
+    private MessengerRestHandler restHandler;
 
-    public MessengerIntentProvider(@NonNull MessengerPlatform platform) {
+    /**
+     * Constructs a {@link MessengerIntentProvider} and binds it to the provided {@code platform}.
+     *
+     * @param platform the {@link MessengerPlatform} managing this provider
+     */
+    public MessengerIntentProvider(final @NonNull MessengerPlatform platform) {
         super(platform);
     }
 
-    public void start(@NonNull Configuration configuration) {
-            this.restHandler = createRestHandler();
-            super.start(configuration);
+    @Override
+    public void start(final @NonNull Configuration configuration) {
+        this.restHandler = createRestHandler();
+        super.start(configuration);
     }
+
 
     @Override
     public MessengerRestHandler getRestHandler() {
         return restHandler;
     }
 
+    /**
+     * Returns the URI of the REST endpoint to register the provider to.
+     */
     @Override
     public String getEndpointURI() {
         return "/messenger/webhook";
     }
 
+    /**
+     * Creates a {@link MessengerRestHandler} used to process incoming Rest requests.
+     */
     @Override
     protected MessengerRestHandler createRestHandler() {
         return new MessengerRestHandler() {
-            // TODO: add validation of post request https://developers.facebook.com/docs/messenger-platform/webhook#security
             @Override
-            public HttpEntity handleParsedContent(@Nonnull List<Header> headers, @Nonnull List<NameValuePair> params, @Nullable JsonElement content) throws RestHandlerException {
+            public HttpEntity handleParsedContent(
+                    final @Nonnull List<Header> headers,
+                    final @Nonnull List<NameValuePair> params,
+                    final @Nullable JsonElement content
+            ) throws RestHandlerException {
                 try {
                     checkArgument(nonNull(content), "Missing content.");
 
@@ -61,14 +85,15 @@ public class MessengerIntentProvider extends WebhookEventProvider<MessengerPlatf
                             .iterator()
                             .forEachRemaining(entry -> handleWebhookEntry(entry));
 
-                    return new StringEntity("RECEIVED", "UTF-8");
+                    return new StringEntity("RECEIVED", StandardCharsets.UTF_8);
                 } catch (NullPointerException | IllegalStateException | IllegalArgumentException e) {
-                    throw new RestHandlerException(403, e.getMessage());
+                    throw new RestHandlerException(HttpStatus.SC_FORBIDDEN, e.getMessage());
                 }
-            }};
+            }
+        };
     }
 
-    private void handleWebhookEntry(JsonElement jsonElement) {
+    private void handleWebhookEntry(final JsonElement jsonElement) {
         if (!jsonElement.isJsonObject()) {
             return;
         }
@@ -86,7 +111,7 @@ public class MessengerIntentProvider extends WebhookEventProvider<MessengerPlatf
             val text = requireNonNull(message.getAsJsonObject().get("text"), "Message has no text").getAsString();
 
             try {
-                StateContext context  = this.xatkitCore.getOrCreateContext(id);
+                StateContext context = this.xatkitCore.getOrCreateContext(id);
                 val recognizedIntent = IntentRecognitionHelper.getRecognizedIntent(text,
                         context, this.getRuntimePlatform().getXatkitCore());
                 this.sendEventInstance(recognizedIntent, context);
